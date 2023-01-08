64fdcfbdfb6523c1e93f9fbe1ae8d8b71f7dd296
githumbi <githumbi74@gmail.com>
2023-01-08 12:22:23 +0300

    clickable button that uses states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { React, useState } from "react";
+ 
+function Button(props) {
+  return <button onClick={props.incrementFunction}>+1</button>;
 }
+
+function Display(props) {
+  return <div>{props.message}</div>;
+}
+
+const App = (props) => {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  return (
+    <>
+      <Button incrementFunction={increment} />
+      <Display message={count}/>
+    </>
+  );
+};
 
 export default App;
